76f23a6777a6dec86802eda4e2e992885ad9e7ac
JuanSanchez39 <juan_sanchez39@hotmail.com>
2023-09-25 12:48:08 -0400

    added template and modified  to have a resume , works testimonials contact ,  installed all dependancies , added an env file with skip preflight since it wouldnt run the builkd w/o it , personal information missing

---
+++ src/resumeData.js
@@ -0,0 +1,106 @@
+let resumeData = {
+    "imagebaseurl":"",
+    "name": "",
+    "role": "",
+    "linkedinId":"Your LinkedIn Id",
+    "skypeid": "Your skypeid",
+    "roleDescription": "",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"",
+          "className":"fa fa-skype"
+        }
+      ],
+    "aboutme":"",
+    "address":"",
+    "website":"",
+    "education":[
+      {
+        "UniversityName":"",
+        "specialization":"",
+        "MonthOfPassing":"",
+        "YearOfPassing":"",
+        "Achievements":""
+      },
+      {
+        "UniversityName":"",
+        "specialization":"",
+        "MonthOfPassing":"",
+        "YearOfPassing":"",
+        "Achievements":""
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"",
+        "specialization":"",
+        "MonthOfLeaving":"",
+        "YearOfLeaving":"",
+        "Achievements":""
+      },
+      {
+        "CompanyName":"",
+        "specialization":"",
+        "MonthOfLeaving":"",
+        "YearOfLeaving":"",
+        "Achievements":""
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"",
+        "name":""
+      },
+      {
+        "description":"",
+        "name":""
+      }
+    ]
+  }
+  
+  export default resumeData
